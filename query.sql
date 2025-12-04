090bf82dd69e29464277f2a4186778125c895ee8
amine <aminear220@gmail.com>
2025-12-04 14:32:40 +0100

    Afficher les accounts avec un balance ≤ 0

--- query.sql
+++ query.sql
@@ -17,34 +17,34 @@ select * from accounts where balance > 10000
 
 select * from accounts where balance <= 0
 
-select * from Transactions where transaction_type = 'debit'
+-- select * from Transactions where transaction_type = 'debit'
 
-select * from Transactions where transaction_type = 'credit'
+-- select * from Transactions where transaction_type = 'credit'
 
-select * from Transactions where accountId = 1
+-- select * from Transactions where accountId = 1
 
-select distinct * from Customers c join accounts a on c.customer_id = a.customerId where a.advisorId = 2
+-- select distinct * from Customers c join accounts a on c.customer_id = a.customerId where a.advisorId = 2
 
-select * from accounts where account_type = 'Savings'
+-- select * from accounts where account_type = 'Savings'
 
-select * from Transactions where amount >= 500
+-- select * from Transactions where amount >= 500
 
-select * from Transactions where amount >= 100 and amount <= 1000
+-- select * from Transactions where amount >= 100 and amount <= 1000
 
-select distinct * from accounts where customerId = 1
+-- select distinct * from accounts where customerId = 1
 
-select * from accounts order by balance asc
+-- select * from accounts order by balance asc
 
-select * from Transactions order by amount desc
-
-
-select * from Transactions order by amount desc limit 5
+-- select * from Transactions order by amount desc
 
 
-select * from Transactions order by transaction_date desc
+-- select * from Transactions order by amount desc limit 5
 
 
-select * from Transactions order by transaction_date desc limit 3
+-- select * from Transactions order by transaction_date desc
 
 
-select a.account_id , c.full_name ,ad.full_name from accounts a join Customers c on a.customerId = c.customer_id join advisors ad on a.advisorId = ad.advisor_id
+-- select * from Transactions order by transaction_date desc limit 3
+
+
+-- select a.account_id , c.full_name ,ad.full_name from accounts a join Customers c on a.customerId = c.customer_id join advisors ad on a.advisorId = ad.advisor_id
